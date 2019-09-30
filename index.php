ba9fcfa7b2caf26789a91af74e88e6cb44ff7cc9
BaileyBelisario <bailey.belisario@trojans.dsu.edu>
2019-09-30 19:51:02 +0000

    Current update to site, made it centered.

---
+++ index.php
@@ -0,0 +1,60 @@
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>DSUComputerClub</title>
+		<link rel="stylesheet" href="styles/main.css">
+		<link rel="stylesheet" href="styles/slicknav.css">
+		<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+		<script src="js/jquery.slicknav.min.js"></script>
+		<script type="text/javascript">
+			$(document).ready(function(){
+				$('#nav_menu').slicknav({prependTo:"#mobilemenu"});
+			});
+		</script>
+	</head>
+
+	<header>
+	</header>
+
+	<nav id="mobilemenu"></nav>
+	<nav id="nav_menu">
+		<ul>
+			<li><a href="http://attendance.dsucomputer.club" class="current">Attendance</a></li>
+			<li><a  href="http://events.dsucomputer.club">Calendar</a></li>
+		</ul>
+	</nav>
+
+	<body>
+		<h1><u><b>DSU Computer Club</b></u></h1>
+		<h5> --- All meetings at 5:30PM --- </h5>
+		<br>
+		<br>
+		<ul><h3>Computer Club</h3>
+			<li>Internships are immensely valuable times for applying skills from school and learning new ones. This week in computer club we'll have 7 individuals giving short and sweet presentations about their internships. </li>
+		<h4>Family Weekend</h4>
+		<li>If you are interested in representing Computer Club at the Family Weekend, please fill out <a href="https://forms.gle/oo6jhhh2ksxUNBKb7">this</a> form.</li>
+		</ul>
+		<br>
+		
+		<br>
+		<br>
+		<ul><h3>Defensive Security Club</h3>
+			<li>DefSec will be going over Active Directory, Group Policy, and Windows based services (SSH, FTP, and others).</li>
+		</ul>
+		<br>
+		<br>
+		<ul><h3>Offensive Security Club</h3>
+			<li>Capture the flag! With conveniently less running! Come solve encrypted challenges and compete against your peers! There will be a while range of challenges for you to try to solve! See you at 5:30 Wednesday!</li>
+		</ul>
+		<br>
+		<br>
+		<ul><h3>Programming Club</h3>
+			<li>This week we will be talking about CodeGolf and we will be giving a CodeGolf challenge during the meeting to win a prize.</li>
+		</ul>
+		<br>
+		<h2>Under Construction</h2>
+		<img src="https://thumbs.gfycat.com/JoyfulAfraidAltiplanochinchillamouse-small.gif"/>
+		<br>
+	</body>
+</html>
