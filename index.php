94a50d58c17516fccbe4e1e1ca0386f5d269403c
BaileyBelisario <bailey.belisario@trojans.dsu.edu>
2020-01-15 00:04:26 +0000

    test python script

--- index.php
+++ index.php
@@ -14,47 +14,94 @@
 		</script>
 	</head>
 
-	<header>
-	</header>
 
-	<nav id="mobilemenu"></nav>
+
+	<!--<nav id="mobilemenu"></nav>
 	<nav id="nav_menu">
 		<ul>
-			<li><a href="http://attendance.dsucomputer.club" class="current">Attendance</a></li>
-			<li><a  href="http://events.dsucomputer.club">Calendar</a></li>
+			<li><a  href="http://events.dsucomputer.club" target="_blank">Calendar</a></li>
 		</ul>
-	</nav>
+	</nav>-->
+
+
 
 	<body>
 		<h1><u><b>DSU Computer Club</b></u></h1>
 		<h5> --- All meetings at 5:30PM --- </h5>
+		<p>Contact Us: <a href="mailto:bailey.belisario@trojans.dsu.edu?subject=Computer%20Club&cc=peter.engels@trojans.dsu.edu,rayanne.liester@trojans.dsu.edu,jordan.oberg@trojans.dsu.edu">Email</a></p>
+
 		<br>
 		<br>
+		<br>
+
 		<ul><h3>Computer Club</h3>
-			<li>Internships are immensely valuable times for applying skills from school and learning new ones. This week in computer club we'll have 7 individuals giving short and sweet presentations about their internships. </li>
-		<h4>Family Weekend</h4>
-		<li>If you are interested in representing Computer Club at the Family Weekend, please fill out <a href="https://forms.gle/oo6jhhh2ksxUNBKb7">this</a> form.</li>
+			<li>
+No meeting this week...
+			</li>
+			<li><p><a href="http://attendance.dsucomputer.club" target="_blank">Attendance</a>&nbsp;&nbsp;&nbsp;<a href="http://events.dsucomputer.club" target="_blank">Events</a></p></li>
 		</ul>
+
 		<br>
-		
 		<br>
-		<br>
+
 		<ul><h3>Defensive Security Club</h3>
-			<li>DefSec will be going over Active Directory, Group Policy, and Windows based services (SSH, FTP, and others).</li>
+			<li>
+No meeting this week...
+			</li>
+<br>
+
+			<li><p>
+				<a href="http://attendance.defsec.club" target="_blank">Attendance</a>
+				&nbsp;&nbsp;&nbsp;
+				IALab: <a href="http://html5.defsec.club" target="_blank">HTML5</a>/<a href="http://flash.defsec.club" target="_blank">Flash</a>
+				&nbsp;&nbsp;&nbsp;
+				<a href="http://drive.defsec.club" target="_blank">Drive</a>
+				&nbsp;&nbsp;&nbsp;
+				<a href="http://youtube.defsec.club" target="_blank">YouTube</a>
+			</p></li>
 		</ul>
+
+
 		<br>
 		<br>
-		<ul><h3>Offensive Security Club</h3>
-			<li>Capture the flag! With conveniently less running! Come solve encrypted challenges and compete against your peers! There will be a while range of challenges for you to try to solve! See you at 5:30 Wednesday!</li>
+
+
+		<ul><h3>Offensive Security Club (11/25)</h3>
+			<li>
+No meeting this week...
+			</li>
+		
+		
+			<li><p><a href="http://attendance.offsec.club" target="_blank">Attendance</a>
+				&nbsp;&nbsp;&nbsp;
+				<a href="https://kernelpoppers.org/" target="_blank">kernelpoppers</a>
+				&nbsp;&nbsp;&nbsp;
+				<a href="https://youtu.be/orDailWz_wc" target="_blank">YouTube</a>
+				&nbsp;&nbsp;&nbsp;
+				<a href="http://resources.offsec.club">Resources</a>
+				&nbsp;&nbsp;&nbsp;
+				<a href="http://ctf.offsec.club">CTF</a>
+			</p>
+		
+			</li>
 		</ul>
+
+
 		<br>
 		<br>
-		<ul><h3>Programming Club</h3>
-			<li>This week we will be talking about CodeGolf and we will be giving a CodeGolf challenge during the meeting to win a prize.</li>
+
+
+		<ul><h3>Programming Club (11/25)</h3>
+			<li>
+No meeting this week...
+			</li>
+			<li><p><a href="https://open.kattis.com/" target="_blank">Kattis</a>&nbsp;&nbsp;&nbsp;<a href="http://progclub.ialab.dsu.edu">CodeGolf</a></p></li>
 		</ul>
+
+
 		<br>
-		<h2>Under Construction</h2>
-		<img src="https://thumbs.gfycat.com/JoyfulAfraidAltiplanochinchillamouse-small.gif"/>
 		<br>
+
+
 	</body>
 </html>
